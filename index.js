52fc10b264e5848139ab4ccb49b8ad8880228a0c
Kamron Kamolov <kamronkamolov00@gmail.com>
2022-01-27 14:54:41 +0500

    index.html vs js

---
+++ index.js
@@ -0,0 +1,13 @@
+
+
+    let form=document.getElementById('form')
+    let name=document.getElementById('name')
+    let email=document.getElementById('email')
+    let pass=document.getElementById('fpass')
+    let check=document.getElementById('check')
+    
+	let btn=document.querySelector('button')
+	
+	form.addEventListener('submit',()=>{
+	   alert("work")    
+	})
